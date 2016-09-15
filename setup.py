00c7b527e78b232a68716fc3ce9358e7973d76ce
Paul Gessinger <hello@paulgessinger.com>
2016-09-15 18:09:47 +0200

    prep for pip

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="gepard-python",
+    version="0.9",
+    install_requires=[
+        "python-dateutil"
+    ],
+    author = 'Hans-Juergen Gessinger',
+    author_email = 'gessinger-hj@gmail.com',
+    url = 'https://github.com/paulgessinger/gepard-python',
+    py_modules=['gepard'],
+    entry_points= {
+    }
+)
