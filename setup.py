88ad7700d2f7041044cbd99abfde1298529d9758
Hans-Jürgen Gessinger <gess@alghero.fritz.box>
2017-01-20 18:04:56 +0100

    preparing pypi publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gepard-python",
-    version="1.0",
+    version="1.0.0",
     install_requires=[
         "python-dateutil"
     ],
@@ -11,5 +11,17 @@ setup(
     url = 'https://github.com/paulgessinger/gepard-python',
     py_modules=['gepard'],
     entry_points= {
-    }
+    },
+    keywords = [
+    "distributed-applications",
+    "events",
+    "semaphores",
+    "locks",
+    "messages",
+    "JavaScript",
+    "Java",
+    "Python",
+    "message"
+    ],
+    description = "Python client for gepard:General purpose communications for distributed applications / Microservices / events, semaphores and messages for JavaScript, Java, Python and PHP",
 )
